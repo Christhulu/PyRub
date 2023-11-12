05aef03e1cc13be18b1a35c35685190fb7fe3151
Chris Alexander <christopher.j.alexander@vanderbilt.edu>
2023-11-12 07:11:45 -0500

    test get methods

--- tests/test_face.py
+++ tests/test_face.py
@@ -6,6 +6,7 @@ class TestFace(unittest.TestCase):
     """
     Subclass for testing our methods
     """ 
+
     #region Constructor Tests
     def test_face_side_length(self):
         face = Face('r', 3)
@@ -23,23 +24,102 @@ class TestFace(unittest.TestCase):
     #endregion Constructor Tests
 
     #region str tests
+    def test_face_str(self):
+        face = Face('r', 3)
+
+        str_result = str(face)
+        self.assertNotEqual(str_result, "")
+
 
     #endregion str tests
 
     #region repr tests
+    def test_face_repr(self):
+        face = Face('r', 3)
+
+        repr_result = repr(face)
+        self.assertNotEqual(repr_result, None)
+
 
     #endregion repr tests
 
     #region To String Tests
+    def test_face_to_string(self):
+        face = Face('r', 3)
+
+        to_string_result = face.face_to_string()
+
+        self.assertNotEqual(to_string_result, "")
+
+    def test_flatten_face(self):
+        pass
+
 
     #endregion To String Tests
     
     #region Face Completeness Tests
+    def test_face_completeness(self):
+        face = Face('r', 3)
+
+        complete = face.validate_cell_uniformity()
+
+        self.assertTrue(complete)
+
+    def test_face_incomplete(self):
+        face = Face('r', 3)
+
+        face.cells[0][1] = "*"
+
+        complete = face.validate_cell_uniformity()
+        self.assertFalse(complete)
 
 
     #endregion Face Completeness Tests
 
     #region Get Methods
+    def test_get_top_row(self):
+        face = Face('r', 3)
+        expected_row = ["r", "r", "r"]
+
+        result = face.get_row(0)
+
+        self.assertListEqual(result, expected_row)
+
+    def test_get_middle_row(self):
+        face = Face('r', 3)
+        expected_row = ["r", "r", "r"]
+
+        result = face.get_row(1)
+        self.assertListEqual(result, expected_row)
+
+    def test_get_bottom_row(self):
+        face = Face('r', 3)
+        expected_row = ["r", "r", "r"]
+
+        result = face.get_row(2)
+        self.assertListEqual(result, expected_row)
+
+    def test_get_left_column(self):
+        face = Face('r', 3)
+        expected_column = ["r", "r", "r"]
+
+        result = face.get_column(0)
+        self.assertListEqual(result, expected_column)
+
+
+    def test_get_middle_column(self):
+        face = Face('r', 3)
+        expected_column = ["r", "r", "r"]
+
+        result = face.get_column(1)
+        self.assertListEqual(result, expected_column)
+
+    def test_get_right_column(self):
+        face = Face('r', 3)
+        expected_column = ["r", "r", "r"]
+
+        result = face.get_column(2)
+        self.assertListEqual(result, expected_column)     
 
     #endregion Get Methods
 
