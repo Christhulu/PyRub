375d682fdaf056a069810200f19eabf06053a981
Chris Alexander <christopher.j.alexander@vanderbilt.edu>
2023-11-14 08:07:45 -0500

    update face tests, add placeholders for replacement tests

--- tests/face.py
+++ tests/face.py
@@ -8,29 +8,25 @@ class TestFace(unittest.TestCase):
     """ 
 
     def setUp(self) -> None:
-        return super().setUp()
+        self.face = Face('r', 3)
 
     #region Constructor Tests
     def test_face_side_length(self):
-        face = Face('r', 3)
-        self.assertEqual(face.side_length, 3, 'Incorrect side length')
+        self.assertEqual(self.face.side_length, 3, 'Incorrect side length')
 
     def test_face_side_color(self):
-        face = Face('r', 3)
-        self.assertEqual(face.color, 'r', 'Incorrect color')
+        self.assertEqual(self.face.color, 'r', 'Incorrect color')
 
     def test_face_array_filled_properly(self):
-        face = Face('r', 3)
         faceList = [['r', 'r', 'r'], ['r', 'r', 'r'], ['r', 'r', 'r']]
-        self.assertListEqual(face, faceList, 'Face not initialized to color properly')
+        self.assertListEqual(self.face.cells, faceList, 'Face not initialized to color properly')
 
     #endregion Constructor Tests
 
     #region str tests
     def test_face_str(self):
-        face = Face('r', 3)
 
-        str_result = str(face)
+        str_result = str(self.face)
         self.assertNotEqual(str_result, "")
 
 
@@ -38,9 +34,8 @@ class TestFace(unittest.TestCase):
 
     #region repr tests
     def test_face_repr(self):
-        face = Face('r', 3)
 
-        repr_result = repr(face)
+        repr_result = repr(self.face)
         self.assertNotEqual(repr_result, None)
 
 
@@ -48,36 +43,29 @@ class TestFace(unittest.TestCase):
 
     #region To String Tests
     def test_face_to_string(self):
-        face = Face('r', 3)
-
-        to_string_result = face.face_to_string()
+        to_string_result = self.face.face_to_string()
 
         self.assertNotEqual(to_string_result, "")
 
     
     def test_flatten_face(self):
-        face = Face('r', 3)
 
         expected_result = ["r", "r", "r", "r", "r", "r", "r", "r", "r"]
-        self.assertListEqual(expected_result, face.list_view)
+        self.assertListEqual(expected_result, self.face.list_view)
 
 
     #endregion To String Tests
     
     #region Face Completeness Tests
     def test_face_completeness(self):
-        face = Face('r', 3)
-
-        complete = face.validate_cell_uniformity()
-
+        complete = self.face.validate_cell_uniformity()
         self.assertTrue(complete)
 
     def test_face_incomplete(self):
-        face = Face('r', 3)
 
-        face.cells[0][0] = "*"
+        self.face.cells[0][0] = "*"
 
-        complete = face.validate_cell_uniformity()
+        complete = self.face.validate_cell_uniformity()
         self.assertFalse(complete)
 
 
@@ -85,67 +73,103 @@ class TestFace(unittest.TestCase):
 
     #region Get Methods
     def test_get_top_row(self):
-        face = Face('r', 3)
         expected_row = ["r", "r", "r"]
 
-        result = face.get_row(0)
+        result = self.face.get_row(0)
 
         self.assertListEqual(result, expected_row)
 
     def test_get_middle_row(self):
-        face = Face('r', 3)
         expected_row = ["r", "r", "r"]
 
-        result = face.get_row(1)
+        result = self.face.get_row(1)
         self.assertListEqual(result, expected_row)
 
     def test_get_bottom_row(self):
-        face = Face('r', 3)
+
         expected_row = ["r", "r", "r"]
 
-        result = face.get_row(2)
+        result = self.face.get_row(2)
         self.assertListEqual(result, expected_row)
 
     def test_get_left_column(self):
-        face = Face('r', 3)
+
         expected_column = ["r", "r", "r"]
 
-        result = face.get_column(0)
+        result = self.face.get_column(0)
         self.assertListEqual(result, expected_column)
 
 
     def test_get_middle_column(self):
-        face = Face('r', 3)
+
         expected_column = ["r", "r", "r"]
 
-        result = face.get_column(1)
+        result = self.face.get_column(1)
         self.assertListEqual(result, expected_column)
 
     def test_get_right_column(self):
-        face = Face('r', 3)
+
         expected_column = ["r", "r", "r"]
 
-        result = face.get_column(2)
+        result = self.face.get_column(2)
         self.assertListEqual(result, expected_column)     
 
     #endregion Get Methods
 
     #region Rotate Methods
-    def test_rotate_CW(self):
+    def test_rotate_CW_row_updated(self):
         
-        face = Face('r', 3)
-        face.cells[0][0] = "*"
-        face.cells[0][1] = "*"
-        face.cells[0][2] = "*"
+        self.face.cells[0][0] = "*"
+        self.face.cells[0][1] = "*"
+        self.face.cells[0][2] = "*"
         
-        face.rotate_CW()
+        self.face.rotate_CW()
 
+        expectedFaceList = [['r', 'r', '*'], ['r', 'r', '*'], ['r', 'r', '*']]
 
+        self.assertListEqual(self.face.cells, expectedFaceList)
+
+  
+
+    def test_rotate_CCW(self):
+
+        self.face.cells[0][0] = "*"
+        self.face.cells[0][1] = "*"
+        self.face.cells[0][2] = "*"
+
+        self.face.rotate_CCW()
+
+        expectedFaceList = [['*', 'r', 'r'], ['*', 'r', 'r'], ['*', 'r', 'r']]
+
+        self.assertListEqual(self.face.cells, expectedFaceList)
 
 
     #endregion Rotate Methods
 
     #region Replace Methods
+    def test_replace_row_with_row(self):
+        pass
+
+    def test_replace_row_with_row_descending(self):
+        pass
+
+    def test_replace_row_with_col(self):
+        pass
+
+    def test_replace_row_with_col_descending(self):
+        pass
+
+    def test_replace_col_with_col(self):
+        pass
+
+    def test_replace_col_with_col_descending(self):
+        pass
+
+    def test_replace_col_with_row(self):
+        pass
+
+    def test_replace_col_with_row_descending(self):
+        pass
 
     #endregion Replace methods
 
