4700122e08b99febf99b11ede205e0d560195bea
Chris Alexander <christopher.j.alexander@vanderbilt.edu>
2023-11-15 09:51:41 -0500

    add a little summary at the end of test run

--- tests/runner.py
+++ tests/runner.py
@@ -1,9 +1,14 @@
 # tests/runner.py
 import unittest
+from unittest import getTestCaseNames
 
 import cube
 import face
 import game
+
+from face import TestFace
+from cube import TestCube
+from game import TestGame
 
 # initialize the test suite
 loader = unittest.TestLoader()
@@ -17,3 +22,16 @@ suite.addTests(loader.loadTestsFromModule(game))
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner(verbosity=3)
 result = runner.run(suite)
+
+face_tests = getTestCaseNames(TestFace)
+cube_tests = getTestCaseNames(TestCube)
+game_tests = getTestCaseNames(TestGame)
+
+total_test_count = len(face_tests) + len(cube_tests) + len(game_tests)
+failed_test_count = len(result.failures)
+successful_test_count = total_test_count - failed_test_count
+
+print("===[Test Suite Summary]===\n")
+print(f"Total Tests: {total_test_count}\n")
+print(f"Successful Tests:\n Count: {successful_test_count}/{total_test_count} \t Percentage: {successful_test_count/total_test_count}%\n")
+print(f"Failed Tests:\n Count: {failed_test_count}/{total_test_count} \t Percentage: {failed_test_count/total_test_count}%\n")
