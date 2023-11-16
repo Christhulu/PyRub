25d94c1bda23693435380901c9e3a95c39e8b156
Chris Alexander <christopher.j.alexander@vanderbilt.edu>
2023-11-16 09:34:42 -0500

    use pyproject.toml instead

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='PyRub',
-    version='1.0',
-    packages=['pyrub',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
-)
