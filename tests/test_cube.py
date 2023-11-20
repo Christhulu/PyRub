ef7dc3f82c758cf2d8e52e827f106eaf31f552f3
Chris Alexander <christopher.j.alexander@vanderbilt.edu>
2023-11-20 10:50:42 -0500

    finish constructor tests

--- tests/test_cube.py
+++ tests/test_cube.py
@@ -55,61 +55,107 @@ class TestCube(unittest.TestCase):
     def test_cube_front_populated(self):
         #Tests that the front face's cells are correctly set
         actual_result = self.cube.front.cells
-        expected_result = [["", "", ""], ["", "", ""], ["", "", ""]]
+        expected_result = [["o", "o", "o"], ["o", "o", "o"], ["o", "o", "o"]]
         
         self.assertListEqual(actual_result, expected_result)
 
     def test_cube_back_initialized(self):
-        pass
+        #Tests that the back's face is created
+        actual_result = self.cube.back is not None and isinstance(self.cube.back, Face)
+        expected_result = True
+        
+        self.assertEqual(expected_result, actual_result)
 
     def test_cube_back_populated(self):
         actual_result = self.cube.front.cells
-        expected_result = [["", "", ""], ["", "", ""], ["", "", ""]]
+        expected_result = [["r", "r", "r"], ["r", "r", "r"], ["r", "r", "r"]]
         
         self.assertListEqual(actual_result, expected_result)
 
     def test_cube_left_initialized(self):
-        pass
+        #Tests that the left's face is created
+        actual_result = self.cube.left is not None and isinstance(self.cube.left, Face)
+        expected_result = True
+        
+        self.assertEqual(expected_result, actual_result)
 
     def test_cube_left_populated(self):
         actual_result = self.cube.front.cells
-        expected_result = [["", "", ""], ["", "", ""], ["", "", ""]]
+        expected_result = [["g", "g", "g"], ["g", "g", "g"], ["g", "g", "g"]]
         
         self.assertListEqual(actual_result, expected_result)   
 
     def test_cube_right_initialized(self):
-        pass
+        #Tests that the right's face is created
+        actual_result = self.cube.right is not None and isinstance(self.cube.right, Face)
+        expected_result = True
+        
+        self.assertEqual(expected_result, actual_result)
 
-    def test_cube_right_populated(self):
+    def test_cube_right_populated(  self):
         actual_result = self.cube.right.cells
-        expected_result = [["", "", ""], ["", "", ""], ["", "", ""]]
+        expected_result = [["b", "b", "b"], ["b", "b", "b"], ["b", "b", "b"]]
         
         self.assertListEqual(actual_result, expected_result)  
 
     def test_cube_top_initialized(self):
-        pass
+        #Tests that the top's face is created
+        actual_result = self.cube.top is not None and isinstance(self.cube.top, Face)
+        expected_result = True
+        
+        self.assertEqual(expected_result, actual_result)
 
     def test_cube_top_populated(self):
         actual_result = self.cube.top.cells
-        expected_result = [["", "", ""], ["", "", ""], ["", "", ""]]
+        expected_result = [["y", "y", "y"], ["y", "y", "y"], ["y", "y", "y"]]
         
         self.assertListEqual(actual_result, expected_result)
 
     def test_cube_bottom_initialized(self):
-        pass
+        #Tests that the bottom's face is created
+        actual_result = self.cube.bottom is not None and isinstance(self.cube.bottom, Face)
+        expected_result = True
+        
+        self.assertEqual(expected_result, actual_result)
 
     def test_cube_bottom_populated(self):
         actual_result = self.cube.bottom.cells
-        expected_result = [["", "", ""], ["", "", ""], ["", "", ""]]
+        expected_result = [["w", "w", "w"], ["w", "w", "w"], ["w", "w", "w"]]
         
         self.assertListEqual(actual_result, expected_result) 
 
     def test_cube_front_back_opposites(self):
-        pass
+        front_color = set(self.cube.front.list_view).pop()
+        back_color = set(self.cube.back.list_view).pop()
+        
+        front_opposite = self.cube.opposites[front_color]
+        back_opposite = self.cube.opposites[back_color]
+
+        opposites = front_opposite == back_color and back_opposite == front_color
+
+        self.assertTrue(opposites)
 
     def test_cube_left_right_opposites(self):
-        pass
+        left_color = set(self.cube.left.list_view).pop()
+        right_color = set(self.cube.right.list_view).pop()
+        
+        left_opposite = self.cube.opposites[left_color]
+        right_opposite = self.cube.opposites[right_color]
 
+        opposites = (left_opposite == right_color) and (right_opposite == left_color)
+
+        self.assertTrue(opposites)
+
+    def test_cube_top_bottom_opposites(self):
+        top_color = set(self.cube.top.list_view).pop()
+        bottom_color = set(self.cube.bottom.list_view).pop()
+        
+        top_opposite = self.cube.opposites[top_color]
+        bottom_opposite = self.cube.opposites[bottom_color]
+
+        opposites = (top_opposite == bottom_color) and (bottom_opposite == top_color)
+
+        self.assertTrue(opposites)
 
     #endregion Constructor Tests
 
